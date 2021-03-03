265ccde40d479a736aa38fb3430d9e26ab9d7cdd
Godrian <57924566+Godrian@users.noreply.github.com>
2021-03-03 20:07:55 +0100

    Add files via upload

--- GomokuPlayer/player.py
+++ GomokuPlayer/player.py
@@ -1,23 +1,25 @@
 import math
 
-import numpy as np
+from time import time
 
-from misc import legalMove, winningTest
+from misc import legalMove, winningTest, rowTest, diagTest
 from gomokuAgent import GomokuAgent
 
 
 class Player(GomokuAgent):
     def move(self, board):
-        print("Player have move: {}".format(self.ID))
+        t = time()
         while True:
-            board_next = board.copy()
-            m, position_x, position_y = self.max(board_next)
+            m, position_x, position_y = self.max(board, 0)
             move_position = (position_x, position_y)
             if legalMove(board, move_position):
+                t1 = time()
+                print("Time to do move: ", t1-t)
                 return move_position
 
-    def max(self, board):
-        max_score = -math.inf
+    def max(self, board, depth):
+        depth += 1
+        max_score = -2
         first_result = winningTest(self.ID, board, self.X_IN_A_LINE)
         second_result = winningTest(-self.ID, board, self.X_IN_A_LINE)
         position_x = None
@@ -33,19 +35,18 @@ class Player(GomokuAgent):
         for i in range(0, self.BOARD_SIZE):
             for j in range(0, self.BOARD_SIZE):
                 if board[i][j] == 0:
-                    print(board[i][j])
                     board[i][j] = self.ID
-                    value, min_x, min_y = self.min(board)
+                    value, min_x, min_y = self.min(board, depth)
                     if value > max_score:
                         max_score = value
-                        position_y = j
                         position_x = i
+                        position_y = j
                     board[i][j] = 0
-        print("Max function returns score:{} X:{} Y:{}".format(max_score,position_x,position_y))
         return max_score, position_x, position_y
 
-    def min(self, board):
-        min_score = math.inf
+    def min(self, board, depth):
+        min_score = 2
+        depth +=1
         first_result = winningTest(self.ID, board, self.X_IN_A_LINE)
         second_result = winningTest(-self.ID, board, self.X_IN_A_LINE)
         position_x = None
@@ -54,8 +55,6 @@ class Player(GomokuAgent):
         if first_result is True and second_result is False:
             return 1, 0, 0
         elif first_result is False and second_result is True:
-            print(board)
-            print("-1")
             return -1, 0, 0
         elif (first_result and second_result) is False and 0 not in board:
             return 0, 0, 0
@@ -64,11 +63,10 @@ class Player(GomokuAgent):
             for j in range(0, self.BOARD_SIZE):
                 if board[i][j] == 0:
                     board[i][j] = -self.ID
-                    value, max_x, max_y = self.max(board)
+                    value, max_x, max_y = self.max(board, depth)
                     if value < min_score:
                         min_score = value
-                        position_y = j
                         position_x = i
-                    board[j][i] = 0
-        print("Max function returns score:{} X:{} Y:{}".format(min_score, position_x, position_y))
+                        position_y = j
+                    board[i][j] = 0
         return min_score, position_x, position_y
